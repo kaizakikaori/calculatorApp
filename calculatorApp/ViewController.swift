b79c316f11a10944e1ba6f1ae87a07e3d376a3b2
VERTEX21 <vertex21@VERTEX21noMacBook-Air.local>
2019-08-15 16:15:27 +0900

    first

---
+++ calculatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculatorApp
+//
+//  Created by VERTEX21 on 2019/08/15.
+//  Copyright © 2019 k-kougi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
