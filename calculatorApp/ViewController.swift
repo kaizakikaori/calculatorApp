aed5149197a033177da73136d8a345fcc99e5848
VERTEX21 <vertex21@VERTEX21noMacBook-Air.local>
2019-08-16 21:06:15 +0900

    修正完了

--- calculatorApp/ViewController.swift
+++ calculatorApp/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-         label.text = ""
+        label.text = ""
     }
     
     //数字ボタン
